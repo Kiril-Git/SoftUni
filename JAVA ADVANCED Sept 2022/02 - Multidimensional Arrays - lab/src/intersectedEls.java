8cf28a67b88df9413084d8ba80922a272b385af8
Kiril Kirilov <kirilov@hotmail.co.uk>
2022-09-26 08:03:58 +0100

    Added to Multidimensional Arrays - Lab

---
+++ JAVA ADVANCED Sept 2022/02 - Multidimensional Arrays - lab/src/intersectedEls.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class intersectedEls {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+
+        Character[][] matrix1 =  readMatrix(scanner, rows, cols);
+        Character[][] matrix2 =  readMatrix(scanner, rows, cols);
+
+        Character[][] intersect = matrix1.clone();
+
+        for (int r = 0; r < rows; r++) {
+
+            for (int i = 0; i < cols; i++) {
+
+                if (matrix1[r][i] != matrix2[r][i])
+                                            intersect[r][i] = '*';
+            }
+        }
+        for (Character[] el : intersect){
+            for (Character i : el){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static Character[][] readMatrix(Scanner scanner, int rows, int cols) {
+
+        Character [][] matrix = new Character[rows][cols];
+        for (int i = 0; i < rows; i++) {
+
+            matrix[i] = Arrays.stream(scanner.nextLine().split(" "))
+                    .map(e -> e.charAt(0))
+                    .toArray(Character[] :: new);
+
+        }
+        return matrix;
+    }
+}
---
+++ JAVA ADVANCED Sept 2022/02 - Multidimensional Arrays - lab/src/matrixElsSum.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class matrixElsSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] input = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int rows = input[0];
+        int cols = input[1];
+        int[][] matrix = readMatrix(scanner, rows, cols);
+
+        int sum = 0;
+        for (int[] r : matrix){
+            sum += Arrays.stream(r).sum();
+        }
+        System.out.println(rows);
+        System.out.println(cols);
+        System.out.println(sum);
+    }
+
+    private static int[][] readMatrix(Scanner scanner, int rows, int cols) {
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            matrix[i] = Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+
+        return matrix;
+    }
+}
