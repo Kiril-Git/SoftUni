30aed36250fb35a951a7c17b4c8cc9e5687eb38c
Kiril Kirilov <kirilov@hotmail.co.uk>
2022-11-18 15:27:59 +0000

    Added to Lab: Inheritance

---
+++ JAVA OPP Oct. 2022/03 Inheritance - Lab/src/multipleInheritance/Animal.java
@@ -0,0 +1,9 @@
+package multipleInheritance;
+
+public class Animal {
+
+
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ JAVA OPP Oct. 2022/03 Inheritance - Lab/src/multipleInheritance/Main.java
@@ -0,0 +1,17 @@
+package multipleInheritance;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+
+
+    }
+}
