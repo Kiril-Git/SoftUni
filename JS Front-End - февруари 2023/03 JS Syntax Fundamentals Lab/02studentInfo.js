19cf174d6da44c4b9c7a331a0c3924462a907f3b
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-02-23 09:30:12 +0000

    JS FRONT-END - ФЕВРУАРИ 2023

---
+++ JS Front-End - февруари 2023/03 JS Syntax Fundamentals Lab/02studentInfo.js
@@ -0,0 +1,9 @@
+function solve(name, age, grade){
+
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`)
+}
+
+
+name= 'Ivan';
+age = 25;
+grade = 4.654;
---
+++ JS Front-End - февруари 2023/03 JS Syntax Fundamentals Lab/test/index.js
@@ -0,0 +1,26 @@
+
+let dayOfWeek = 'Sunday';
+
+switch(dayOfWeek){
+    case 'Monday':
+        console.log('Week day!');
+        break;
+        case 'Tuesday':
+        console.log('Week day!');
+        break;
+        case 'Wednesday':
+        console.log('Week day!');
+        break;
+        case 'Thursday':
+        console.log('Week day!');
+        break;
+        case 'Friday':
+        console.log('Week day!');
+        break;
+        case 'saturday':
+        console.log('Weekend!');
+        break;
+        case 'Sunday':
+        console.log('Weekend!');
+        break;
+}
