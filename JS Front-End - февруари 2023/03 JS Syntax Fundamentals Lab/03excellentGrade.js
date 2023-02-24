8d238a42d415b37de4b137f099e95ef84d03fc5a
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-02-24 15:07:34 +0000

    Added to JS Syntax Fundamentals - Lab

---
+++ JS Front-End - февруари 2023/03 JS Syntax Fundamentals Lab/03excellentGrade.js
@@ -0,0 +1,11 @@
+function solve(number){
+    if(number >= 5.5){
+        console.log('Excellent');
+    }else{
+        console.log('Not excellent');
+    }
+}
+
+solve(5.7)
+solve(5.2)
+solve(5.5)
