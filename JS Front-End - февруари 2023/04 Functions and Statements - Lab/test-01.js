aa09ef93e92836c9d91f741f20509655fb8645cb
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-04 10:58:00 +0000

    launched - Functions and Statements - Lab

---
+++ JS Front-End - февруари 2023/04 Functions and Statements - Lab/test-01.js
@@ -0,0 +1,16 @@
+ const printStars =  function(count){
+
+    return '*'.repeat(count);
+ }
+
+ console.log(
+    printStars(3)                     //  print 3 starts  
+ )
+
+ for (let i = 0; i < 8; i++) {
+
+    console.log(
+        printStars(5)
+    );
+    console.log('donga ');
+ }
---
+++ JS Front-End - февруари 2023/04 Functions and Statements - Lab/test-02.js
@@ -0,0 +1,18 @@
+ function printHeader(){
+
+   console.log('- - - - Page Header')
+ }
+ function printContent(){
+
+   console.log('- - - - Page Content')
+ }
+ function printFooter(){
+
+   console.log('- - - - Page Footer')
+ }
+ function printBody(){
+   printHeader();
+   printContent();
+   printFooter();
+ }
+ printBody()
