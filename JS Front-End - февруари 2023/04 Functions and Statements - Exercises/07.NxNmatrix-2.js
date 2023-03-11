0e65130efd4a8c975ddc9cdfd01d102a33d433bb
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-11 21:20:44 +0000

    Added to Functions and Statements - Exercises

---
+++ JS Front-End - февруари 2023/04 Functions and Statements - Exercises/07.NxNmatrix-2.js
@@ -0,0 +1,5 @@
+function solve(n){
+
+   new Array(n).fill(new Array(n).fill(n)).forEach(row => console.log(row.join(' ')))
+}
+solve(3)
--- JS Front-End - февруари 2023/04 Functions and Statements - Exercises/07.NxNmatrix.js
+++ JS Front-End - февруари 2023/04 Functions and Statements - Exercises/07.NxNmatrix.js
@@ -4,7 +4,7 @@ function solve(n){
 
     for (let i = 0; i < n; i++) {
 
-        arr.push(n);
+        arr.push(n);                //  arr += n + ' ';
 
     }
     for (let j = 0; j < n; j++) {
