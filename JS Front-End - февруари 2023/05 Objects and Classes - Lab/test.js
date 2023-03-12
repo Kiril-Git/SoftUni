2766e4f6746629c01b88995f6bc02e0fc794446a
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-12 08:20:48 +0000

    Launch: Objects and Classes - Lab

---
+++ JS Front-End - февруари 2023/05 Objects and Classes - Lab/test.js
@@ -0,0 +1,10 @@
+let person = {nickName: 'Donga', age: 52, hairColor: 'black', size: [23,6] }
+
+console.log(person)
+console.log(person.nickName)
+console.log(person.size[0])
+
+person.nickName = 'Buzema';
+person.size = [23, 4];
+console.log(person.size)
+
