51fe13ff8339638f1de1beb3b94da450d0ea8f3f
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-15 09:06:46 +0000

    Added to Objects and Classes - Lab

---
+++ JS Front-End - февруари 2023/05 Objects and Classes - Lab/08.AddressBook.js
@@ -0,0 +1,25 @@
+function printAddressBook(input){
+
+    let addressBook = {};
+
+    for (const el of input) {
+
+        let [name, address] = el.split(':');
+
+            addressBook[name] = address;
+    }
+
+    let sortedBook = Object.keys(addressBook).sort();
+
+    for (const el of sortedBook) {
+
+        console.log(`${el} -> ${addressBook[el]}`)
+    }
+}
+
+printAddressBook(
+    ['Tim:Doe Crossing',
+    'Bill:Nelson Place',
+    'Peter:Carlyle Ave',
+    'Bill:Ornery Rd']
+)
