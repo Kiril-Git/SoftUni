7fef46d146ccfaf33a44ec9fde7beda4951d7bd4
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-25 16:07:37 +0000

    Added to DOM and Events - Lab

--- JS Front-End - февруари 2023/06 DOM and Events - Lab/01. Sum-Numbers/SumNumbers.js
+++ JS Front-End - февруари 2023/06 DOM and Events - Lab/01. Sum-Numbers/SumNumbers.js
@@ -1,3 +1,13 @@
 function calc() {
-    // TODO: sum = num1 + num2
+
+    let input1  = document.getElementById('num1')
+    let input2 = document.getElementById('num2')
+
+    let firstN = Number(input1.value)
+    let secondN = Number(input2.value)
+
+    let sum = firstN + secondN
+
+    let result = document.getElementById('sum')
+    result.value = sum
 }
--- JS Front-End - февруари 2023/06 DOM and Events - Lab/test/index.js
+++ JS Front-End - февруари 2023/06 DOM and Events - Lab/test/index.js
@@ -2,3 +2,12 @@ function sum (){
     console.log(5+5);
 }
 sum();
+
+const liElements = document.getElementsByTagName('li');
+const textInput = document.getElementById('text-input');
+
+for (const el of liElements) {
+
+    el.innerHTML += ' <p>Custom Paragraph<p/ > '
+    el.style.background = 'red'
+}
