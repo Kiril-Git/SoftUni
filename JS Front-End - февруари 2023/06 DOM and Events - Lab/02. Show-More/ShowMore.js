701e4ef6d49d305c84ef3433103d31481fca4e4b
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-27 13:18:52 +0100

    Added to DOM and Events - Lab

--- JS Front-End - февруари 2023/06 DOM and Events - Lab/02. Show-More/ShowMore.js
+++ JS Front-End - февруари 2023/06 DOM and Events - Lab/02. Show-More/ShowMore.js
@@ -1,3 +1,8 @@
 function showText() {
-    // TODO
+    // TODO:
+    let textInput = document.getElementById('text');
+    textInput.style.display = 'inline';     // ? = 'block';
+
+    let textHidden = document.getElementById('more');
+    textHidden.style.display = 'none';
 }
--- JS Front-End - февруари 2023/06 DOM and Events - Lab/03. Collect-List-Items/CollectListItems.js
+++ JS Front-End - февруари 2023/06 DOM and Events - Lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
     // TODO
+    let list = document.getElementById('items')
+
+    let textArea = document.getElementById('result')
+    textArea.textContent = list.textContent
 }
