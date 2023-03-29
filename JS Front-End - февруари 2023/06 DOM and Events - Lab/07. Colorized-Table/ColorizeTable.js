0262b5fdb68cb36a14453e7194993de5b4baea65
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-03-29 17:19:35 +0100

    DOM and Events - Lab - completed

--- JS Front-End - февруари 2023/06 DOM and Events - Lab/07. Colorized-Table/ColorizeTable.js
+++ JS Front-End - февруари 2023/06 DOM and Events - Lab/07. Colorized-Table/ColorizeTable.js
@@ -1,3 +1,5 @@
 function colorize() {
     // TODO
+let tableLine = Array.from(document.querySelectorAll('tr:nth-of-type(2n)'))
+    tableLine.forEach((td) => td.style.background = 'teal')
 }
--- JS Front-End - февруари 2023/06 DOM and Events - Lab/08. Sum-Table/SumTable.js
+++ JS Front-End - февруари 2023/06 DOM and Events - Lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,14 @@
 function sumTable() {
 
+    let price = document.querySelectorAll('tr td:nth-of-type(2)')
+
+    let sum = Array.from(price).reduce((a, x) => {
+        let temp = Number(x.textContent) || 0;
+        return a + temp;
+    }, 0);
+
+    let total = document.getElementById('sum')
+
+    total.textContent = sum
+
 }
