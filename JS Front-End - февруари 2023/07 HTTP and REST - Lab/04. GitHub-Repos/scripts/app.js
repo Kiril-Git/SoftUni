975c8c92ed1ea1dd6801abab74abbe59e604919d
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-04-09 09:30:58 +0100

    Added to HTTP and REST - Lab

--- JS Front-End - февруари 2023/07 HTTP and REST - Lab/04. GitHub-Repos/scripts/app.js
+++ JS Front-End - февруари 2023/07 HTTP and REST - Lab/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,16 @@
 function loadRepos() {
+   const BASE_URL = 'https://api.github.com/users/testnakov/repos'
+
+   const resultContainer = document.getElementById('res')
+
+   fetch(BASE_URL, {method: 'GET'})
+       .then((res) => res.text())
+       .then((data) => {
+          resultContainer.textContent = data;
+       })
+       .catch((err) => {
+          console.error(err)
+       })
+
    console.log("TODO...");
 }
