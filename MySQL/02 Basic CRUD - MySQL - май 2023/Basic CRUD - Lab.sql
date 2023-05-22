01d17dd438e1161315db7e02e08ea8fe6c266c45
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-05-22 17:10:19 +0100

    Added to Basic CRUD - Lab

--- MySQL/02 Basic CRUD - MySQL - май 2023/Basic CRUD - Lab.sql
+++ MySQL/02 Basic CRUD - MySQL - май 2023/Basic CRUD - Lab.sql
@@ -39,7 +39,91 @@ SELECT
 FROM
     employees;
 
+-- 			- SELECT WHERE salary > 1000
+
+SELECT 
+    concat_ws(`first_name`, `last_name`) AS 'Full Name', job_title AS 'Job Title', `id` AS 'No.', `salary` AS 'Salary'
+FROM
+    employees WHERE salary > 1000;
+
 -- -
+
+SELECT DISTINCT
+    department_id
+FROM
+    employees;
+
+-- -
+
+SELECT 
+    last_name, department_id
+FROM
+    employees
+WHERE
+    department_id = 1;
+
+-- -
+SELECT id, last_name, salary FROM employees WHERE salary <= 1500;
+
+-- -
+
+SELECT 
+    last_name, department_id
+FROM
+    employees
+WHERE
+    NOT (department_id = 2 OR department_id = 4);
+
+-- -
+
+SELECT 
+    last_name, salary
+FROM
+    employees
+WHERE
+    salary BETWEEN 1000 AND 2000;
+
+-- -
+
+SELECT 
+    last_name, department_id
+FROM
+    employees
+WHERE
+    department_id NOT IN (1, 2, 3);
+    
+    -- 2
+    
+SELECT 
+    id,
+    CONCAT_WS(' ',`first_name`, `last_name`) AS 'full_name',
+    job_title,
+    salary
+FROM
+    employees
+WHERE
+    salary > 1000
+    ORDER BY id;
+
+-- 			- Views – Example
+
+CREATE VIEW v_hr_result_set AS
+    SELECT 
+        CONCAT_WS(' ', first_name, last_name) AS 'Full Name', salary
+    FROM
+        employees
+    ORDER BY department_id;
+    
+    SELECT * FROM v_hr_result_set;
+    
+    -- -
+
+
+
+
+
+
+
 
 
 
