1cdf191f51123ae3145d2e7dbbaa456a75bf617c
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-05-22 08:44:43 +0100

    Added то Basic CRUD

--- MySQL/02 Basic CRUD - MySQL - май 2023/Basic CRUD - Lab.sql
+++ MySQL/02 Basic CRUD - MySQL - май 2023/Basic CRUD - Lab.sql
@@ -1,1 +1,46 @@
-SELECT * FROM employees;
+SELECT 
+    *
+FROM
+    employees;
+-- - 
+
+SELECT 
+    id, first_name, last_name, job_title
+FROM
+    employees;
+
+-- -
+ 
+SELECT 
+    id, first_name, last_name, job_title
+FROM
+    employees
+ORDER BY id;
+
+-- -
+
+SELECT 
+    id AS 'ID', first_name AS 'First Name', last_name AS 'Last Name', job_title
+FROM
+    employees
+ORDER BY id;
+
+-- 			- concatinating 
+
+SELECT 
+    concat(`first_name`,' ',`last_name`) AS 'Full Name', job_title AS 'Job Title', `id` AS 'No.'
+FROM
+    employees;
+
+-- 			- concat with separator
+
+SELECT 
+    concat_ws(`first_name`, `last_name`) AS 'Full Name', job_title AS 'Job Title', `id` AS 'No.'
+FROM
+    employees;
+
+-- -
+
+
+
+
