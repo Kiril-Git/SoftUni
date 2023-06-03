94acee424b676e30ce4e2f608873507effd92de3
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-03 08:47:19 +0100

    Data Aggregation - Lab - Cpmpleted

--- MySQL/04 Data Aggregation/Data Aggregation - Lab.sql
+++ MySQL/04 Data Aggregation/Data Aggregation - Lab.sql
@@ -8,5 +8,81 @@ department_id ORDER BY department_id;
 SELECT department_id,  COUNT(id) AS 'Number of employees' from employees GROUP BY 
 department_id ORDER BY department_id, 'Number of employees';
 
+--		-
+ALTER TABLE employees
+CHANGE COLUMN last_name last_name VARCHAR(30);
+
+
+UPDATE employees 
+SET last_name = NULL 
+WHERE id in(2, 4, 6, 8);					
+
+--  	-
+
+ALTER TABLE employees
+CHANGE COLUMN salary salary DOUBLE;
+
+
+UPDATE employees 
+SET salary = NULL 
+WHERE id in(2, 4, 6, 8);
+
+--		-
+
+SELECT department_id, MAX(first_name) FROM employees GROUP BY department_id;
+
+--		2
+
+SELECT department_id, ROUND(AVG(salary), 2) AS 'Average Salary' FROM employees GROUP BY 
+department_id ORDER BY department_id;
+
+--		3
+
+SELECT 
+    department_id, ROUND(MIN(salary), 2) AS 'Min Salary'
+FROM
+    employees
+WHERE
+    salary > 800
+GROUP BY department_id
+ORDER BY department_id
+LIMIT 1;
+
+--		-
+
+SELECT * FROM employees ORDER BY last_name DESC;
+
+--		-
+
+SELECT 
+    department_id, MIN(salary)
+FROM
+    employees
+GROUP BY department_id
+HAVING
+    MIN(salary) > 800;
+    
+    --		4
+    
+    SELECT  COUNT( price) FROM products  WHERE category_id = 2 AND price > 8;
+    
+    --		5
+    
+  SELECT 
+    category_id, ROUND(AVG(price), 2) AS 'Average Price',
+    ROUND(MIN(price), 2) AS 'Cheapest Product',
+    ROUND(MAX(price), 2) AS 'Most Expencive Product'
+FROM
+    products
+GROUP BY category_id
+ORDER BY category_id;
+
+
+
+
+
+
+
+
 
 
