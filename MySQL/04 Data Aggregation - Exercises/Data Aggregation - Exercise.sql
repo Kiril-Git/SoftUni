c88e630567e4c1af98918ca44d8856ca3feed411
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-06 08:57:23 +0100

    Data Aggregation - Exercise - Completed

--- MySQL/04 Data Aggregation - Exercises/Data Aggregation - Exercise.sql
+++ MySQL/04 Data Aggregation - Exercises/Data Aggregation - Exercise.sql
@@ -32,6 +32,97 @@ FROM
 GROUP BY `deposit_group`
 ORDER BY total_sum;
 
+--		6
+
+     SELECT `deposit_group`, SUM(`deposit_amount`) AS 'total_sum'
+FROM
+    `wizzard_deposits` WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY `deposit_group` ORDER BY deposit_group; 
+
+--		7
+
+SELECT `deposit_group`, SUM(`deposit_amount`) AS 'total_sum' FROM `wizzard_deposits` 
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY `deposit_group` 
+HAVING `total_sum` < 150000 
+ORDER BY total_sum; 
+
+--		8
+
+SELECT deposit_group, magic_wand_creator, MIN(deposit_charge) AS 'min_deposit_charge'
+FROM wizzard_deposits
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY magic_wand_creator ASC, deposit_group;
+
+--		9
+
+SELECT 
+    CASE
+        WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
+        WHEN `age` BETWEEN 11 AND 20 THEN '[11-20]'
+        WHEN `age` BETWEEN 21 AND 30 THEN '[21-30]'
+        WHEN `age` BETWEEN 31 AND 40 THEN '[31-40]'
+        WHEN `age` BETWEEN 41 AND 50 THEN '[41-50]'
+        WHEN `age` BETWEEN 51 AND 60 THEN '[51-60]'
+        ELSE '[61+]'
+    END AS 'age_group',
+    COUNT(`age`) AS 'wizard_count'
+FROM
+    `wizzard_deposits`
+GROUP BY `age_group`
+ORDER BY `wizard_count`;
+
+--		10
+
+SELECT left(first_name, 1) AS 'first_letter' FROM wizzard_deposits  WHERE 
+deposit_group = 'Troll Chest' GROUP BY first_letter ORDER BY first_letter;
+
+--		11
+
+SELECT 
+    deposit_group, is_deposit_expired, AVG(deposit_interest) AS 'average_interest'
+FROM wizzard_deposits 
+    WHERE deposit_start_date > '1985-01-01'
+    GROUP BY deposit_group, is_deposit_expired
+    ORDER BY deposit_group DESC, is_deposit_expired ASC; 
+
+--		12
+
+SELECT department_id, MIN(salary) AS 'minimum_salary' FROM employees
+WHERE department_id IN (2, 5, 7) AND hire_date > '2000-01-01'
+GROUP BY department_id 
+ORDER BY department_id ASC; 
+
+
+SELECT department_id, MIN(salary) AS 'minimum_salary' FROM employees
+WHERE hire_date > '2000-01-01'
+GROUP BY department_id 
+HAVING department_id IN (2, 5, 7) 
+ORDER BY department_id ASC; 
+
+--		13
+
+CREATE TABLE high_paid_employees 
+AS SELECT employee_id, department_id, salary, manager_id 
+FROM employees
+WHERE salary > 30000;
+
+DELETE FROM high_paid_employees WHERE manager_id = 42;
+
+UPDATE high_paid_employees
+SET salary = salary + 5000
+WHERE department_id = 1;
+
+SELECT department_id, AVG(salary) AS 'avg_salary' FROM high_paid_employees
+GROUP BY department_id
+ORDER BY department_id ASC;
+
+
+
+
+
+
+
 
 
 
