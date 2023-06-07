26de7d5d33d1eee76ff4863e258d30d5ae125e80
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-07 08:14:08 +0100

    update

--- MySQL/05 Table Relations/Table Relations - Lab.sql
+++ MySQL/05 Table Relations/Table Relations - Lab.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE mountains_and_peaks;
+
+CREATE TABLE mountains(
+id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR (30) 
+);
+
+CREATE TABLE peaks(
+id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR (30),
+mountains_id INT,
+FOREIGN KEY (mountains_id) REFERENCES mountains (id) ON DELETE CASCADE
+);
