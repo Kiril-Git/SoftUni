8beaf91fd45b9d169cead4b5faa0b91660610f99
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-08 15:35:38 +0100

    Table Relations - Lab - completed

--- MySQL/05 Table Relations/Table Relations - Lab.sql
+++ MySQL/05 Table Relations/Table Relations - Lab.sql
@@ -2,17 +2,17 @@ CREATE DATABASE mountains_and_peaks;
 
 --		1
 
-
-CREATE TABLE mountains(
-id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `mountains`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR (30) 
 );
 
-CREATE TABLE peaks(
-id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `peaks`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR (30),
-mountains_id INT,
-FOREIGN KEY (mountains_id) REFERENCES mountains (id) ON DELETE CASCADE
+`mountains_id` INT,
+CONSTRAINT `peaks_mountains_id`
+FOREIGN KEY (`mountains_id`) REFERENCES mountains (`id`) ON DELETE CASCADE
 );
 
 INSERT INTO mountains (`name`) VALUES 
@@ -37,8 +37,89 @@ INSERT INTO peaks (`name`, mountains_id) VALUES
 SELECT p.id, p.name, mountains.name FROM peaks AS p
 JOIN mountains ON p.mountains_id = mountains.id;
 
+ --		2
+ USE camp;
+ 
+SELECT 
+    driver_id,
+    vehicle_type,
+    CONCAT_WS(' ',first_name, last_name) AS driver_name
+FROM
+    vehicles
+        JOIN
+    campers ON driver_id = campers.id;
+ 
+--		3
 
-
+SELECT 
+    starting_point AS 'route_starting_point',
+    end_point AS 'route_ending_point',
+    leader_id,
+    CONCAT_WS(' ', first_name, last_name) AS leader_name
+FROM
+    routes
+        JOIN
+    campers ON leader_id = campers.id;
+    
+    --		4
+    
+    USE mountains_and_peaks;
+    
+    DELETE FROM mountains WHERE id = 1;
+    
+    --		53
+    
+    CREATE DATABASE project_management_db;
+    
+    CREATE TABLE employees(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	`first_name` VARCHAR (30) ,
+	`last_name` VARCHAR (30) ,
+	project_id INT(11),
+    
+	CONSTRAINT `fk_project_id`
+	FOREIGN KEY (`project_id`) REFERENCES projects (`id`) ON DELETE CASCADE
+    );
+    
+    CREATE TABLE clients(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	`client_name` VARCHAR (100) 
+    
+    );
+    
+       CREATE TABLE projects(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+	client_id INT(11),
+	project_lead_id INT(11),
+	
+    CONSTRAINT `fk_client_id`
+	FOREIGN KEY (`client_id`) REFERENCES clients (`id`) ON DELETE CASCADE
+    );
+    
+    ALTER TABLE projects ADD
+    CONSTRAINT `fk_project_lead_id`
+	FOREIGN KEY (`project_lead_id`) REFERENCES employees (`id`) ON DELETE CASCADE;
+    
+    SELECT * FROM projects;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
