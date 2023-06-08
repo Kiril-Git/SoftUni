67e587e1a0cdfc49791c8d97f9fa210aa136b872
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-08 08:36:27 +0100

    update

--- MySQL/05 Table Relations/Table Relations - Lab.sql
+++ MySQL/05 Table Relations/Table Relations - Lab.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE mountains_and_peaks;
 
+--		1
+
+
 CREATE TABLE mountains(
 id INT PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR (30) 
@@ -11,3 +14,32 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 mountains_id INT,
 FOREIGN KEY (mountains_id) REFERENCES mountains (id) ON DELETE CASCADE
 );
+
+INSERT INTO mountains (`name`) VALUES 
+('Rila'), 
+('Pirin');
+
+SELECT * FROM mountains;
+
+INSERT INTO peaks (`name`, mountains_id) VALUES 
+('Musala', 1), 
+('Vihren', 2);
+
+SELECT * FROM peaks;
+
+--		
+
+SELECT * FROM peaks JOIN mountains ON peaks.mountains_id = mountains_id;
+
+INSERT INTO peaks (`name`, mountains_id) VALUES 
+('Муратов връх', 2);
+
+SELECT p.id, p.name, mountains.name FROM peaks AS p
+JOIN mountains ON p.mountains_id = mountains.id;
+
+
+
+
+
+
+
