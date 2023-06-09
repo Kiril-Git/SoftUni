25af443322d221fa5a45bd678d5afcf90f94ff07
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-09 08:55:39 +0100

    Added  to Table Relations - Exercise

--- MySQL/05 Table Relations  - Exercises/Table Relations - Exercise.sql
+++ MySQL/05 Table Relations  - Exercises/Table Relations - Exercise.sql
@@ -4,8 +4,8 @@ CREATE DATABASE one_to_one_relationship;
 
 CREATE TABLE people(
 person_id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(30),
-salary DOUBLE(7, 2), 
+first_name VARCHAR(30) NOT NULL,
+salary DECIMAL(10, 2), 
 passport_id INT,
 
 FOREIGN KEY (passport_id) REFERENCES passports(passport_id) ON DELETE CASCADE
@@ -13,7 +13,7 @@ FOREIGN KEY (passport_id) REFERENCES passports(passport_id) ON DELETE CASCADE
 
 CREATE TABLE passports(
 passport_id INT PRIMARY KEY AUTO_INCREMENT,
-passport_number VARCHAR(30)
+passport_number VARCHAR(10) UNIQUE
 );
 
 SELECT * FROM people;
@@ -28,7 +28,38 @@ INSERT INTO people(first_name, salary, passport_id) VALUE
 ('Tom', 56100.00, 103),
 ('Yana', 60200.00, 101);
 
+--		2
 
+CREATE DATABASE one_to_many_relationship;
+
+CREATE TABLE manufacturers(
+manufacturer_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(10) UNIQUE,
+established_on DATE
+);
+
+CREATE TABLE models(
+model_id INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(10) UNIQUE,
+manufacturer_id INT,
+
+FOREIGN KEY (manufacturer_id) REFERENCES manufacturers(manufacturer_id) ON DELETE CASCADE
+);
+
+INSERT INTO manufacturers(name, established_on) VALUE
+('BMW', '1916-03-01'),
+('Tesla', '2003-01-01'),
+('Lada', '1966-05-01');
+
+INSERT INTO models(model_id, name, manufacturer_id) VALUE
+(101, 'X1', 1),
+(102, 'i6', 1),
+(103, 'Model S', 2),
+(104, 'Model X', 2),
+(105, 'Model 3', 2),
+(106, 'Nova', 3);
+
+SELECT * FROM models;
 
 
 
