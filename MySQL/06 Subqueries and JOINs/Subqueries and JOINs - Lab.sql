a86335aa466c32f49df06ca146f83d9e5d8b69fc
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-12 11:37:23 +0100

    Subqueries and JOINs - Lab - completed

--- MySQL/06 Subqueries and JOINs/Subqueries and JOINs - Lab.sql
+++ MySQL/06 Subqueries and JOINs/Subqueries and JOINs - Lab.sql
@@ -38,7 +38,7 @@ UNION
 SELECT first_name, departments.name from employees 
 RIGHT JOIN departments ON departments.id = employees.department_id;
 
---		1
+--		1:
 
 SELECT 
 employee_id,
@@ -46,11 +46,23 @@ CONCAT_WS(' ',first_name, last_name) AS full_name,
 departments.department_id,
 departments.name AS 'department_name'
 FROM employees
-RIGHT JOIN
+INNER JOIN
 departments ON departments.manager_id = employees.employee_id
 ORDER BY employees.employee_id LIMIT 5;
 
+--		2: 
 
+SELECT addresses.town_id, towns.name AS 'town_name', address_text 
+FROM addresses
+RIGHT JOIN towns ON towns.town_id = addresses.town_id
+WHERE towns.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY town_id, address_id;
+
+--		3
+
+SELECT employee_id, first_name, last_name, department_id, salary 
+FROM employees
+WHERE manager_id IS NULL;
  
  
 
