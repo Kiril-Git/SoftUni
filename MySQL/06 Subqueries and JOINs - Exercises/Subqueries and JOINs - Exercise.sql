3a9b6f37fd4e4ca11c2af4ddb7415d49cd46bf0c
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-16 13:45:35 +0100

    update

--- MySQL/06 Subqueries and JOINs - Exercises/Subqueries and JOINs - Exercise.sql
+++ MySQL/06 Subqueries and JOINs - Exercises/Subqueries and JOINs - Exercise.sql
@@ -122,15 +122,37 @@ ORDER BY elevation  DESC;
 
 --		13
 
-SELECT mc.country_code, COUNT(mountain_id) AS 'mountain_range '
+SELECT mc.country_code, COUNT(mountain_range) 
 FROM mountains AS m
 JOIN mountains_countries AS mc ON m.id = mc.mountain_id
 WHERE country_code IN ('BG', 'US', 'RU') 
 GROUP BY country_code
-ORDER BY COUNT(mountain_range) DESC
+ORDER BY COUNT(mountain_range) DESC;
 
 --		14
 
+SELECT country_name,	river_name
+FROM countries AS c
+LEFT JOIN countries_rivers AS cr ON c.country_code = cr.country_code
+LEFT JOIN rivers AS r ON cr.river_id = r.id
+WHERE continent_code = 'AF'
+ORDER BY country_name ASC;
+
+--		15
+
+SELECT continent_code,	currency_code,	COUNT(*) AS 'currency_usage' 
+FROM countries AS c
+GROUP BY continent_code, currency_code
+HAVING currency_usage > 1
+AND currency_usage =  (SELECT COUNT(*) AS currencies_count
+FROM countries AS c2
+WHERE c2.continent_code = c.continent_code
+GROUP BY c2.currency_code
+ORDER BY currencies_count DESC
+LIMIT 1)
+ORDER BY continent_code ASC, currency_code ASC
+
+--		16
 
 
 
