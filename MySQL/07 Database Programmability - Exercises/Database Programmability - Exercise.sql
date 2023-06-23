87821cb9334213f5bde7e90d60da49d678137136
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-23 08:25:08 +0100

    update

--- MySQL/07 Database Programmability - Exercises/Database Programmability - Exercise.sql
+++ MySQL/07 Database Programmability - Exercises/Database Programmability - Exercise.sql
@@ -7,13 +7,16 @@ DELIMITER $$
 
 # DROP PROCEDURE usp_get_employees_salary_above_35000;
 
+#   judge ↓
 CREATE PROCEDURE usp_get_employees_salary_above_35000()
 BEGIN
 
     SELECT first_name,	last_name FROM employees WHERE salary > 35000
     ORDER BY first_name ASC, last_name ASC , employee_id ASC;
 
-END $$
+END
+#   ↑   judge
+$$
 DELIMITER ;
 ;
 
