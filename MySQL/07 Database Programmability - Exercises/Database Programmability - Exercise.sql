fcd9736212fd91767e3b701fe085ba2495fc3e9a
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-23 08:16:57 +0100

    update

---
+++ MySQL/07 Database Programmability - Exercises/Database Programmability - Exercise.sql
@@ -0,0 +1,20 @@
+SET GLOBAL log_bin_trust_function_creators = 1;
+SET SQL_SAFE_UPDATES = 0;
+
+--		1
+
+DELIMITER $$
+
+# DROP PROCEDURE usp_get_employees_salary_above_35000;
+
+CREATE PROCEDURE usp_get_employees_salary_above_35000()
+BEGIN
+
+    SELECT first_name,	last_name FROM employees WHERE salary > 35000
+    ORDER BY first_name ASC, last_name ASC , employee_id ASC;
+
+END $$
+DELIMITER ;
+;
+
+CALL usp_get_employees_salary_above_35000();
