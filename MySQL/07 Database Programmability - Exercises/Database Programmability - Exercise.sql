2e332f4f7c953691e8a065831b647948444326bd
Kiril Kirilov <kirilov@hotmail.co.uk>
2023-06-24 10:32:15 +0100

    Added to Database Programmability - Exercise

--- MySQL/07 Database Programmability - Exercises/Database Programmability - Exercise.sql
+++ MySQL/07 Database Programmability - Exercises/Database Programmability - Exercise.sql
@@ -21,3 +21,56 @@ DELIMITER ;
 ;
 
 CALL usp_get_employees_salary_above_35000();
+
+--      2
+
+DELIMITER $$
+
+CREATE PROCEDURE usp_get_employees_salary_above (num DOUBLE(19,4))
+BEGIN
+
+    SELECT first_name,	last_name FROM employees WHERE salary >= num
+    ORDER BY first_name ASC, last_name ASC , employee_id ASC;
+
+END
+$$
+DELIMITER ;
+;
+
+CALL usp_get_employees_salary_above (50000);
+
+--      3
+
+DELIMITER $$
+
+#        DROP PROCEDURE usp_get_towns_starting_with;
+
+CREATE PROCEDURE usp_get_towns_starting_with  (str VARCHAR(10) )
+BEGIN
+
+    SELECT name AS 'town_name' FROM towns WHERE name LIKE CONCAT(str, '%')
+    ORDER BY name ;
+
+END
+$$
+DELIMITER ;
+;
+
+CALL usp_get_towns_starting_with  ('CAL');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
